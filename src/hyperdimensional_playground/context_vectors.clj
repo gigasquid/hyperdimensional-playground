d23060aa15a56cc0e2c72ac7794cb308bee92d25
gigasquid <cmeier@gigasquidsoftware.com>
2016-02-10 18:13:17 -0500

    reasoning

--- src/hyperdimensional_playground/context_vectors.clj
+++ src/hyperdimensional_playground/context_vectors.clj
@@ -9,7 +9,12 @@
                                   CoreAnnotations$TextAnnotation CoreAnnotations$PartOfSpeechAnnotation
                                   CoreAnnotations$NamedEntityTagAnnotation)))
 
-
+(defn rand-hv []
+  (let [hv (m/new-sparse-array [sz])
+        n (* 0.1 sz)]
+    (dotimes [i n]
+      (m/mset! hv (rand-int sz) 1))
+    hv))
 
 (defn cosine-sim [v1 v2]
   (let [norm1 (ml/norm v1)
@@ -85,7 +90,6 @@
                                             wonder-wings-nouns old-fashioned-nouns fairy-godmother-nouns
                                             golden-spears-nouns red-cap-nouns))
 
-  (count )
   (take 100 grimm-nouns)
   (count grimm-nouns)
   (count fairy-tales-nouns) ;=> 136606
@@ -97,7 +101,7 @@
 
 )
 (def grimm-nouns (clojure.edn/read-string (slurp "resources/fairy-tales-nouns-grimm.edn")))
-(def fairy-tales-nouns (set () grimm-nouns))
+(def fairy-tales-nouns (set grimm-nouns))
 
 
 ;;;; construct a matrix of 10322 x 10,000 and use random columns of 20
@@ -205,88 +209,185 @@
 
 
 
- ;(process-book "resources/grimm_fairy_tales.txt")
+                                        ;(process-book "resources/grimm_fairy_tales.txt")
 
- (def x (remove nil? (sim-report)))
- (second x)
- (take 100 x)
- (sort-by :cosine (filter #( :cosine % ) x))
+  (def x (remove nil? (sim-report)))
+  (second x)
+  (take 100 x)
+  (sort-by :cosine (filter #( :cosine % ) x))
 
- (def results (sort-by :cosine (filter #(and (:cosine %) (< 0.4 (:cosine %))) x)))
-
-
- (take 100 (reverse results))
-
- (sim-for-word "fairy" 0.4)
- (sim-for-word "prince" 0.5)
- (sim-for-word "king" 0.01)
- (sim-for-word "queen" 0.4)
- (sim-for-word "witch" 0.2)
- (sim-for-word "heart" 0.1)
- (sim-for-word "apples" 0.1)
- (sim-for-word "guard" 0.1)
- (sim-for-word "eyes" 0.2)
- (sim-for-word "monster" 0.5)
- (sim-for-word "time" 0.6)
- (unlike-for-word "king" 0.04) ;=> "tusks"
- (sim-for-word "tusks" 0.01)
- (sim-for-word "goat" 0.1)
- (sim-for-vec (get-word-vec "fairy") 0.5)
-
- (sort-by :cosine[(compare-word-vecs "king" "queen")
-                  (compare-word-vecs "king" "prince")
-                  (compare-word-vecs "king" "princess")
-                  (compare-word-vecs "king" "guard")
-                  (compare-word-vecs "king" "goat")])
-;; ({:word1 "king", :word2 "goat", :cosine 0.1509151478896664}
-;;  {:word1 "king", :word2 "guard", :cosine 0.16098893367403827}
-;;  {:word1 "king", :word2 "queen", :cosine 0.49470535530616655}
-;;  {:word1 "king", :word2 "prince", :cosine 0.5832521795716931}
-;;  {:word1 "king", :word2 "princess", :cosine 0.5836922474743367})
+  (def results (sort-by :cosine (filter #(and (:cosine %) (< 0.4 (:cosine %))) x)))
 
 
+  (take 100 (reverse results))
 
- (cosine-sim (get-word-vec "boy") (get-word-vec "king")) ;=> 0.42996397142253145
- (cosine-sim (get-word-vec "boy") (get-word-vec "goat")) ;=> 0.10263384987428588
- (cosine-sim
-  (mean-add (get-word-vec "boy") (get-word-vec "gold"))
-  (get-word-vec "king")) ;=> 0.5876251031366048
- (sim-for-vec (mean-add (get-word-vec "boy") (get-word-vec "gold")) 0.5)
+  (sim-for-word "fairy" 0.4)
+  (sim-for-word "prince" 0.5)
+  (sim-for-word "king" 0.01)
+  (sim-for-word "queen" 0.4)
+  (sim-for-word "witch" 0.1)
+  (sim-for-word "heart" 0.1)
+  (sim-for-word "apples" 0.1)
+  (sim-for-word "guard" 0.1)
+  (sim-for-word "eyes" 0.2)
+  (sim-for-word "monster" 0.4)
+  (sim-for-word "time" 0.6)
+  (unlike-for-word "king" 0.04) ;=> "tusks"
+  (sim-for-word "tusks" 0.01)
+  (sim-for-word "goat" 0.1)
+  (sim-for-vec (get-word-vec "fairy") 0.5)
 
-
- (cosine-sim (get-word-vec "girl") (get-word-vec "queen")) ;=> 0.3356337018948302
- (cosine-sim (get-word-vec "girl") (get-word-vec "goat")) ;=> 0.10061278928161434
- (cosine-sim
-  (mean-add (get-word-vec "girl") (get-word-vec "gold"))
-  (get-word-vec "queen")) ;=> 0.437410495036485
- (cosine-sim
-  (mean-add (get-word-vec "girl") (get-word-vec "prince"))
-  (get-word-vec "queen")) ;=> 0.49567588785560884
-
- (cosine-sim (get-word-vec "queen")
-             (mean-add (get-word-vec "woman") (wv-subtract "king" "man"))) ;=>0.5659832204544486
-
- (cosine-sim (mean-add (get-word-vec "giant") (get-word-vec "boy")) (get-word-vec "jack")) ;=>0.4491473187787431
- (cosine-sim (get-word-vec "boy") (get-word-vec "jack")) ;=> 0.33102858702785953
-
-
-(compare-word-vecs "prince" "princess")
-(compare-word-vecs "apple" "tree")
-(compare-word-vecs "apple" "round")
-
-(compare-word-vecs "girl" "boy") ;=> {:word1 "girl", :word2 "boy", :cosine 0.5127662367009652}
-(compare-word-vecs "dragon" "fire")
-(compare-word-vecs "sea" "boat")
-(compare-word-vecs "sea" "bird") ;=> {:word1 "sea", :word2 "bird", :cosine 0.4001370016455863}
-(compare-word-vecs "sea" "shoe") ;=> {:word1 "sea", :word2 "shoe", :cosine 0.11207265084843468}
-(compare-word-vecs "witch" "magic")
-
-(get noun-idx "too")
-(second (string/split (slurp "resources/grimm_fairy_tales.txt") #"\."))
-
-(process-book "resources/grimm_fairy_tales.txt")
+  (sort-by :cosine[(compare-word-vecs "king" "queen")
+                   (compare-word-vecs "king" "prince")
+                   (compare-word-vecs "king" "princess")
+                   (compare-word-vecs "king" "guard")
+                   (compare-word-vecs "king" "goat")])
+  ;; ({:word1 "king", :word2 "goat", :cosine 0.1509151478896664}
+  ;;  {:word1 "king", :word2 "guard", :cosine 0.16098893367403827}
+  ;;  {:word1 "king", :word2 "queen", :cosine 0.49470535530616655}
+  ;;  {:word1 "king", :word2 "prince", :cosine 0.5832521795716931}
+  ;;  {:word1 "king", :word2 "princess", :cosine 0.5836922474743367})
 
 
 
+  (cosine-sim (get-word-vec "boy") (get-word-vec "king")) ;=> 0.42996397142253145
+  (cosine-sim (get-word-vec "boy") (get-word-vec "goat")) ;=> 0.10263384987428588
+  (cosine-sim
+   (mean-add (get-word-vec "boy") (get-word-vec "gold"))
+   (get-word-vec "king")) ;=> 0.5876251031366048
+  (sim-for-vec (mean-add (get-word-vec "boy") (get-word-vec "gold")) 0.5)
 
-)
+
+  (cosine-sim (get-word-vec "girl") (get-word-vec "queen")) ;=> 0.3356337018948302
+  (cosine-sim (get-word-vec "girl") (get-word-vec "goat")) ;=> 0.10061278928161434
+  (cosine-sim
+   (mean-add (get-word-vec "girl") (get-word-vec "gold"))
+   (get-word-vec "queen")) ;=> 0.437410495036485
+  (cosine-sim
+   (mean-add (get-word-vec "girl") (get-word-vec "prince"))
+   (get-word-vec "queen")) ;=> 0.49567588785560884
+
+  (cosine-sim (get-word-vec "queen")
+              (mean-add (get-word-vec "woman") (wv-subtract "king" "man"))) ;=>0.5659832204544486
+
+  (cosine-sim (mean-add (get-word-vec "giant") (get-word-vec "boy")) (get-word-vec "jack")) ;=>0.4491473187787431
+  (cosine-sim (get-word-vec "boy") (get-word-vec "jack")) ;=> 0.33102858702785953
+
+  (cosine-sim (wv-add "frog" "princess") (get-word-vec "prince")) ;=> 0.5231641991974249
+  (cosine-sim (wv-add "frog" "witch") (get-word-vec "prince")) ;=> 0.28231826406597205
+  (cosine-sim (wv-add "frog" "magic") (get-word-vec "prince")) ;=> 0.24898297475282194
+  (cosine-sim (get-word-vec "frog") (get-word-vec "prince")) ;=> 0.1507782796891884
+  (cosine-sim (get-word-vec "father")
+              (mean-add (get-word-vec "man") (wv-subtract "mother" "woman"))) ;=>0.5959841177719538
+  (cosine-sim (get-word-vec "father") (get-word-vec "woman"));=>
+                                        ;0.39586439279718383
+
+  (take 400 noun-idx)
+
+  (compare-word-vecs "prince" "princess")
+  (compare-word-vecs "apple" "tree")
+  (compare-word-vecs "apple" "round")
+
+  (compare-word-vecs "girl" "boy") ;=> {:word1 "girl", :word2 "boy", :cosine 0.5127662367009652}
+  (compare-word-vecs "dragon" "fire")
+  (compare-word-vecs "sea" "boat")
+  (compare-word-vecs "sea" "bird")
+  (compare-word-vecs "sea" "shoe")
+  (compare-word-vecs "witch" "magic")
+
+
+  ;; hansel is the brother of gretel
+  ;; B*H + B*G
+  (def hansel-brother-of-gretel
+    (mean-add
+     (xor-mul (get-word-vec "brother") (get-word-vec "hansel"))
+     (xor-mul (get-word-vec "brother") (get-word-vec "gretel"))))
+  (def jack-brother-of-hansel
+    (mean-add
+     (xor-mul (get-word-vec "brother") (get-word-vec "jack"))
+     (xor-mul (get-word-vec "brother") (get-word-vec "hansel"))))
+
+  (def facts (mean-add hansel-brother-of-gretel
+                       jack-brother-of-hansel))
+
+  ;; is hansel the brother of gretel?
+
+  (cosine-sim
+   (get-word-vec "hansel")
+   (xor-mul (mean-add (get-word-vec "brother") (get-word-vec "gretel"))
+            facts)) ;=> 0.542518819404095
+
+  ;; is john the brother of gretel?
+  (cosine-sim
+   (get-word-vec "john")
+   (xor-mul (mean-add (get-word-vec "brother") (get-word-vec "gretel"))
+            facts)) ;=> 0.13036770808976186
+
+  ;; is cinderella the brother of gretel ?
+  (cosine-sim
+   (get-word-vec "cinderella")
+   (xor-mul (mean-add (get-word-vec "brother") (get-word-vec "gretel"))
+            facts)) ;=> 0.12327219767046675
+
+  ;; is jack the brother of gretel ?
+  (cosine-sim
+   (get-word-vec "jack")
+   (xor-mul (mean-add (get-word-vec "brother") (get-word-vec "gretel"))
+            facts));=> 0.3354538693328559
+
+
+  ;; let's add some more facts
+
+  ;; gretel is the sister of hansel
+                                        ; S*G + S*H
+  (def gretel-sister-of-hansel
+    (mean-add
+     (xor-mul (get-word-vec "sister") (get-word-vec "gretel"))
+     (xor-mul (get-word-vec "sister") (get-word-vec "hansel"))))
+
+
+  ;; gretel is the sister of jack
+                                        ; S*G + S*H
+  (def gretel-sister-of-jack
+    (mean-add
+     (xor-mul (get-word-vec "sister") (get-word-vec "gretel"))
+     (xor-mul (get-word-vec "sister") (get-word-vec "jack"))))
+
+;;; a sibling is a brother and sister
+
+  (def siblings (rand-hv))
+  (def siblings-brother-sister
+    (mean-add (xor-mul siblings (get-word-vec "brother")) (xor-mul siblings (get-word-vec "sister"))))
+
+
+  (def facts (mean-add hansel-brother-of-gretel
+                       jack-brother-of-hansel
+                       gretel-sister-of-jack
+                       gretel-sister-of-hansel
+                       siblings-brother-sister))
+
+  ;; are hansel and gretel siblings?
+  (cosine-sim
+   (mean-add (get-word-vec "hansel") (get-word-vec "gretel"))
+   (xor-mul siblings facts)) ;=> 0.6003227988919575
+
+  ;; are john and cinderella siblings?
+  (cosine-sim
+   (mean-add (get-word-vec "john") (get-word-vec "cinderella"))
+   (xor-mul siblings facts)) ;=> 0.26025663087008655
+
+  ;; are jack and gretel siblings?
+  (cosine-sim
+   (mean-add (get-word-vec "jack") (get-word-vec "gretel"))
+   (xor-mul siblings facts)) ;=> 0.5462252603770413
+
+  ;; are jack and hansel siblings?
+  (cosine-sim
+   (mean-add (get-word-vec "jack") (get-word-vec "hansel"))
+   (xor-mul siblings facts)) ;=> 0.5431168503661485
+
+  ;; are jack and cinderella siblings?
+  (cosine-sim
+   (mean-add (get-word-vec "hansel") (get-word-vec "cinderella"))
+   (xor-mul siblings facts)) ;=>0.44419839153931734
+  )
